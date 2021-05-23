c3aa2902a36deab55744969fb61688953d7be1b4
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-05-22 23:01:08 -0400

    Add section for Dictionaries

--- examples/dictionaries.jl
+++ examples/dictionaries.jl
@@ -1,4 +1,11 @@
-# Examples of dictionaries
+#=
+Examples of dictionaries.
+=#
 
+# Dictionary using key => value
 contacts = Dict("Bart" => 14, "Marge" => 39, "Homer" => 44)
 println(contacts["Bart"])
+
+# Dictionary using (key, value)
+contacts2 = Dict([("Lisa", 16), ("Krusty", 28)])
+println(contacts2["Lisa"])
