c82cf6820cbb9f242fec1a79ca6e9a76b828586c
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2021-05-23 22:56:34 -0400

    Add section for Tuples

--- examples/tuples.jl
+++ examples/tuples.jl
@@ -1,10 +1,19 @@
-# Examples of tuples
+#=
+Examples of tuples.
+=#
 
+# Tuple of strings
 people = ("Marge", "Homer", "Bart", "Lisa")
 println(people)
 println(people[2])
 
+# Tuple with one item
+item = (2,)
+println(item[1])
+
+# Named tuple
 vehicle = (make="Ford", model="Bronco", year=2021)
 println(vehicle.make)
+println(vehicle[1])
 println(vehicle.model)
 println(vehicle.year)
